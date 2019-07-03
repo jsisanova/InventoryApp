d40efe7486ccf5c46894bd6b8afa2bb82e6a4a68
jsisanova <jana.sisanova@gmail.com>
2019-07-04 00:50:24 +0200

    Define basic layout of the app

---
+++ app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
@@ -0,0 +1,123 @@
+package com.example.android.inventoryapp;
+
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+/**
+ * Allows user to create a new pet or edit an existing one.
+ */
+public class EditorActivity extends AppCompatActivity {
+
+    /** EditText field to enter the book name */
+    private EditText mNameEditText;
+
+    /** EditText field to enter the book price */
+    private EditText mPriceEditText;
+
+    /** EditText field to enter the book quantity */
+    private EditText mQuantityEditText;
+
+    /** EditText field to enter the book supplier name */
+    private Spinner mSupplierNameSpinner;
+
+    /** EditText field to enter the book supplier phone */
+    private EditText mSupplierPhoneEditText;
+
+    /**
+     * Supplier name of the book. The possible valid values are:
+     * 0 for Baker & Taylor, 1 for TAN Books, 2 for Casemate.
+     */
+    private int mSupplierName = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        // Find all relevant views that we will need to read user input from
+        mNameEditText = (EditText) findViewById(R.id.edit_book_name);
+        mPriceEditText = (EditText) findViewById(R.id.edit_book_price);
+        mQuantityEditText = (EditText) findViewById(R.id.edit_book_quantity);
+        mSupplierNameSpinner = (Spinner) findViewById(R.id.spinner_supplier_name);
+        mSupplierPhoneEditText = (EditText) findViewById(R.id.edit_supplier_phone);
+
+        setupSpinner();
+    }
+
+    /**
+     * Setup the dropdown spinner that allows the user to select the gender of the pet.
+     */
+    private void setupSpinner() {
+        // Create adapter for spinner. The list options are from the String array it will use
+        // the spinner will use the default layout
+        ArrayAdapter supplierNameSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.array_supplier_name_options, android.R.layout.simple_spinner_item);
+
+        // Specify dropdown layout style - simple list view with 1 item per line
+        supplierNameSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        // Apply the adapter to the spinner
+        mSupplierNameSpinner.setAdapter(supplierNameSpinnerAdapter);
+
+        // Set the integer mSelected to the constant values
+        mSupplierNameSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selection)) {
+                    if (selection.equals(getString(R.string.supplier_name_tan_books))) {
+                        mSupplierName = 1; // TAN Books
+                    } else if (selection.equals(getString(R.string.supplier_name_casemate))) {
+                        mSupplierName = 2; // Casemate
+                    } else {
+                        mSupplierName = 0; // Baker & Taylor
+                    }
+                }
+            }
+
+            // Because AdapterView is an abstract class, onNothingSelected must be defined
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                mSupplierName = 0; // Unknown
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // Navigate back to parent activity (CatalogActivity)
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
