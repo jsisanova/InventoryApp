c02ef08964ad2ffee3ca1367722066258dc230d3
jsisanova <jana.sisanova@gmail.com>
2019-07-16 21:23:29 +0200

    Add empty view to the ListView

--- app/src/main/java/com/example/android/inventoryapp/BookCursorAdapter.java
+++ app/src/main/java/com/example/android/inventoryapp/BookCursorAdapter.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
-import com.example.android.inventoryapp.data.BookContract;
 import com.example.android.inventoryapp.data.BookContract.BookEntry;
 
 /**
