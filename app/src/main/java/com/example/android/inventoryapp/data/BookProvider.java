55bfbff21b4777f7a5bd3d217ca413683e0ca958
jsisanova <jana.sisanova@gmail.com>
2019-07-16 11:23:53 +0200

    Implement ContentProvider query() method

--- app/src/main/java/com/example/android/inventoryapp/data/BookProvider.java
+++ app/src/main/java/com/example/android/inventoryapp/data/BookProvider.java
@@ -1,10 +1,14 @@
 package com.example.android.inventoryapp.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+
+import com.example.android.inventoryapp.data.BookContract.BookEntry;
 
 /**
  * {@link ContentProvider} for Inventory app.
@@ -71,7 +75,57 @@ public class BookProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        return null;
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        // This cursor will hold the result of the query
+        Cursor cursor;
+
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case BOOKS:
+                // For the BOOKS code, query the books table directly with the given
+                // projection, selection, selection arguments, and sort order. The cursor
+                // could contain multiple rows of the books table.
+                cursor = database.query(
+                        BookEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            case BOOK_ID:
+                // For the BOOK_ID code, extract out the ID from the URI.
+                // For an example URI such as "content://com.example.android.books/books/3",
+                // the selection will be "_id=?" and the selection argument will be a
+                // String array containing the actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in the selection
+                // arguments that will fill in the "?". Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+                selection = BookEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                // This will perform a query on the books table where the _id equals 3 to return a
+                // Cursor containing that row of the table.
+                cursor = database.query(
+                        BookEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
     }
 
     /**
