7fd9c80b7168da2a19646f208153244218b8d788
jsisanova <jana.sisanova@gmail.com>
2019-07-16 14:47:08 +0200

    Implement ContentProvider getType() method

--- app/src/main/java/com/example/android/inventoryapp/data/BookContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/BookContract.java
@@ -1,5 +1,6 @@
 package com.example.android.inventoryapp.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -49,6 +50,18 @@ public final class BookContract {
          * (which contains the scheme and the content authority) to the PATH segment.
          */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_BOOKS);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of books.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single book.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
 
         // Name of database table for books
         public final static String TABLE_NAME = "books";
